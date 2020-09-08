55428f308ca598d542d45f0e8306bea220975c9c
Hau Pham <hauph@hotmail.com>
2020-09-08 17:50:54 +0700

    '132'

---
+++ src/server/middlewares/upload-metafield.js
@@ -0,0 +1,4 @@
+module.exports = function uploadMetaField(address, img) {
+  console.log("img", img);
+  console.log("address", address);
+};
---
+++ src/server/routes/admin.js
@@ -0,0 +1,61 @@
+const Router = require("koa-router");
+const AdminApi = new Router();
+const bodyParser = require("koa-bodyparser");
+const asyncBusboy = require("async-busboy");
+
+const AppStatus = require("../middlewares/app-status");
+const LoadAppSettings = require("../middlewares/load-app-settings");
+const uploadMetafield = require("../middlewares/upload-metafield");
+
+/**
+ * ROUTE FOR HANDLE APP STATUS================================================================
+ */
+
+AdminApi.get("/app-status", async (ctx) => {
+  const { shop } = ctx.session;
+  const _data = await AppStatus.GetAppStatus(shop);
+  ctx.body = _data;
+});
+
+AdminApi.post("/app-status", bodyParser(), async (ctx) => {
+  const { shop } = ctx.session;
+  const bodyData = ctx.request.body;
+  const _data = await AppStatus.UpdateAppStatus(shop, bodyData);
+  ctx.body = _data;
+});
+
+/**
+ * ============================================================================================
+ */
+
+/**
+ * ROUTE FOR HANDLE CHECK STORE URL WITH IT'S SESSION==========================================
+ */
+AdminApi.post("/check-store", bodyParser(), async (ctx) => {
+  const { shop } = ctx.session;
+  const { parentUrl } = ctx.request.body;
+  ctx.body = {
+    status: "success",
+    valid: shop == parentUrl,
+  };
+});
+/**
+ * ============================================================================================
+ */
+
+AdminApi.get("/load-app-settings", async (ctx) => {
+  const { shop, accessToken } = ctx.session;
+  const _data = await LoadAppSettings({ shop, accessToken });
+  ctx.body = _data;
+});
+
+AdminApi.post("/test", bodyParser(), async (ctx) => {
+  const { address } = ctx.request.body;
+  const { image } = ctx.request.file;
+  console.log("image", image);
+  console.log("ctx.request.body", ctx.request);
+  const _data = await uploadMetafield({ address, image });
+  ctx.body = _data;
+});
+
+module.exports = AdminApi;
